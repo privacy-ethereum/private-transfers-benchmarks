e07a8876f95b7da4a693f4cc7715dbbad6742980
John Guilding <jwguilding@gmail.com>
2026-02-09 16:46:18 -0300

    fix: CI
    - fmt
    - add CI secret

--- test/RailgunTransact.t.sol
+++ test/RailgunTransact.t.sol
@@ -22,8 +22,7 @@ interface IRailgunRelay {
 // Reference tx: 0x105e408f09685adccb1554a325710e90859efcd488fdb2912c8974e73b803cbd
 // Gas cost: cast receipt 0x105e408f09685adccb1554a325710e90859efcd488fdb2912c8974e73b803cbd gasUsed --rpc-url $ETH_RPC_URL
 contract RailgunSendBenchmark is Test {
-    IRailgunRelay constant RELAY =
-        IRailgunRelay(0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9);
+    IRailgunRelay constant RELAY = IRailgunRelay(0xFA7093CDD9EE6932B4eb2c9e1cde7CE00B1FA4b9);
 
     address constant SENDER = 0x0D0Efc24db8fe005e24271c6F823CAC22B0641D8;
 
@@ -35,7 +34,8 @@ contract RailgunSendBenchmark is Test {
     uint256 constant TX_GAS_PRICE = 157_243_696;
 
     function test_send() public {
-        vm.createSelectFork(vm.envString("ETH_RPC_URL"), FORK_BLOCK);
+        string memory rpcUrl = vm.envOr("ETH_RPC_URL", string("https://eth.llamarpc.com"));
+        vm.createSelectFork(rpcUrl, FORK_BLOCK);
         vm.txGasPrice(TX_GAS_PRICE);
 
         Transaction[] memory txs = new Transaction[](1);
@@ -59,20 +59,12 @@ contract RailgunSendBenchmark is Test {
             }),
             b: G2Point({
                 x: [
-                    uint256(
-                        0x022de54da17e30e78420c0cc02b0b38dd955562d36e77bbf80b3f7d945f20062
-                    ),
-                    uint256(
-                        0x0937ada831923756d078a862c16c4b8c177952f4e8a588405f1a8b14f8c6f96f
-                    )
+                    uint256(0x022de54da17e30e78420c0cc02b0b38dd955562d36e77bbf80b3f7d945f20062),
+                    uint256(0x0937ada831923756d078a862c16c4b8c177952f4e8a588405f1a8b14f8c6f96f)
                 ],
                 y: [
-                    uint256(
-                        0x1d796199025622d2b6a16ea9d61db03fbe347c51af8e3d7a5fcb7b10fd713f6a
-                    ),
-                    uint256(
-                        0x1931d5ef9ae87beec74b1f6e68dc41c274f7074954504e7ae61a6b88327fac1e
-                    )
+                    uint256(0x1d796199025622d2b6a16ea9d61db03fbe347c51af8e3d7a5fcb7b10fd713f6a),
+                    uint256(0x1931d5ef9ae87beec74b1f6e68dc41c274f7074954504e7ae61a6b88327fac1e)
                 ]
             }),
             c: G1Point({
@@ -82,38 +74,20 @@ contract RailgunSendBenchmark is Test {
         });
 
         bytes32[] memory nullifiers = new bytes32[](1);
-        nullifiers[
-            0
-        ] = 0x1859b778ec78a2eb608f2ec51a3c5fe8e9bab7a5f19a21044966227b2de12c6f;
+        nullifiers[0] = 0x1859b778ec78a2eb608f2ec51a3c5fe8e9bab7a5f19a21044966227b2de12c6f;
 
         bytes32[] memory commitments = new bytes32[](3);
-        commitments[
-            0
-        ] = 0x2ff10322d6b36b7b4f0062515c568c00f919a7fc6fbce0e958ba05fa44c73e80;
-        commitments[
-            1
-        ] = 0x2530747fc11a38e51bf951fc9cb6aa2bfd29bd0a9939192a03e1f996520f4b22;
-        commitments[
-            2
-        ] = 0x1ae2ff2f4d3e1dcb52b127193bc2806c7bc7c949936867e121557b1ce6eb7584;
+        commitments[0] = 0x2ff10322d6b36b7b4f0062515c568c00f919a7fc6fbce0e958ba05fa44c73e80;
+        commitments[1] = 0x2530747fc11a38e51bf951fc9cb6aa2bfd29bd0a9939192a03e1f996520f4b22;
+        commitments[2] = 0x1ae2ff2f4d3e1dcb52b127193bc2806c7bc7c949936867e121557b1ce6eb7584;
 
-        CommitmentCiphertext[] memory ciphertexts = new CommitmentCiphertext[](
-            2
-        );
+        CommitmentCiphertext[] memory ciphertexts = new CommitmentCiphertext[](2);
         ciphertexts[0] = CommitmentCiphertext({
             ciphertext: [
-                bytes32(
-                    0x4f01dc2f8cd4f194d37dc3e81c4c50ed9856e4661d51a8cca19dc1c5f968b559
-                ),
-                bytes32(
-                    0x026c971af7058f3ce0d8e5c780738ef9199bd6985a911380ac07f96f8be3c579
-                ),
-                bytes32(
-                    0xc05d076bdfb5bd2d9723d9a75ac98032a0587551f91caa2482fc8578260a8323
-                ),
-                bytes32(
-                    0x5516c564e7f32556ee2fb8bf74d21412cc637f4e1ea34143f29dbf46207c44c5
-                )
+                bytes32(0x4f01dc2f8cd4f194d37dc3e81c4c50ed9856e4661d51a8cca19dc1c5f968b559),
+                bytes32(0x026c971af7058f3ce0d8e5c780738ef9199bd6985a911380ac07f96f8be3c579),
+                bytes32(0xc05d076bdfb5bd2d9723d9a75ac98032a0587551f91caa2482fc8578260a8323),
+                bytes32(0x5516c564e7f32556ee2fb8bf74d21412cc637f4e1ea34143f29dbf46207c44c5)
             ],
             blindedSenderViewingKey: 0x3bd01f88b170da516e2797b923d13f809713d79666de845bb22e7275a39c4966,
             blindedReceiverViewingKey: 0xae3daf0c538abb6d93da3f0198b3fb1b5bf29df2911fc5cb2e53b23b29ac3759,
@@ -122,18 +96,10 @@ contract RailgunSendBenchmark is Test {
         });
         ciphertexts[1] = CommitmentCiphertext({
             ciphertext: [
-                bytes32(
-                    0x8739eda54b121b727f3692b482a471bf9b7e8832b9cc4db9f21e8642afe946b4
-                ),
-                bytes32(
-                    0x9b911f2d28388f225d222d55b7e124d872e9ce22eca707ae07bb863df524f074
-                ),
-                bytes32(
-                    0xd1b06bd6aad30e79899071bcd3353065d3fcd14bbcc42313e24a193e2f7fa708
-                ),
-                bytes32(
-                    0x94163ca9bff74ce0f3594f7906d0fd181628ca2ea737d2dde24627087a9eee91
-                )
+                bytes32(0x8739eda54b121b727f3692b482a471bf9b7e8832b9cc4db9f21e8642afe946b4),
+                bytes32(0x9b911f2d28388f225d222d55b7e124d872e9ce22eca707ae07bb863df524f074),
+                bytes32(0xd1b06bd6aad30e79899071bcd3353065d3fcd14bbcc42313e24a193e2f7fa708),
+                bytes32(0x94163ca9bff74ce0f3594f7906d0fd181628ca2ea737d2dde24627087a9eee91)
             ],
             blindedSenderViewingKey: 0xbd722e2a55d4d9ddc9dd4925f3d1f1664601fabcd2acb94e62e979e6620c2397,
             blindedReceiverViewingKey: 0xbd722e2a55d4d9ddc9dd4925f3d1f1664601fabcd2acb94e62e979e6620c2397,
@@ -152,25 +118,20 @@ contract RailgunSendBenchmark is Test {
         });
 
         CommitmentPreimage memory unshieldPreimage = CommitmentPreimage({
-            npk: bytes32(
-                uint256(uint160(0x52CCD390416d0C68A57EBF2b48112F71A8083bDD))
-            ),
+            npk: bytes32(uint256(uint160(0x52CCD390416d0C68A57EBF2b48112F71A8083bDD))),
             token: TokenData({
-                tokenType: TokenType.ERC20,
-                tokenAddress: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
-                tokenSubID: 0
+                tokenType: TokenType.ERC20, tokenAddress: 0xdAC17F958D2ee523a2206206994597C13D831ec7, tokenSubID: 0
             }),
             value: 31_826_566_416
         });
 
-        return
-            Transaction({
-                proof: proof,
-                merkleRoot: 0x29cee84a6ded1b1a70e3aecc4c98cc7c409e1fe4ae00546139409b08bb8aaede,
-                nullifiers: nullifiers,
-                commitments: commitments,
-                boundParams: boundParams,
-                unshieldPreimage: unshieldPreimage
-            });
+        return Transaction({
+            proof: proof,
+            merkleRoot: 0x29cee84a6ded1b1a70e3aecc4c98cc7c409e1fe4ae00546139409b08bb8aaede,
+            nullifiers: nullifiers,
+            commitments: commitments,
+            boundParams: boundParams,
+            unshieldPreimage: unshieldPreimage
+        });
     }
 }
