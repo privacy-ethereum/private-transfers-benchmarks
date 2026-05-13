fb36e5ef057e064068f1b239bac5de0816343711
Nico Serrano <nnicosp@hotmail.com>
2026-05-13 18:07:23 -0500

    feat(benchmarks): add worm (#188)

---
+++ subgraph/tests/worm/weth-withdraw.test.ts
@@ -0,0 +1,129 @@
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { assert, afterAll, describe, beforeAll, clearStore, test } from "matchstick-as/assembly/index";
+
+import { BETH_CONTRACT_ADDRESS, handleWithdrawal } from "../../src/worm/weth";
+
+import { createWithdrawEvent } from "./utils";
+
+const WITHDRAW_TRANSACTION_HASH = Bytes.fromHexString("0xa16081f360e3847006db660bae1c6d1b2e17ec2a");
+const AMOUNT = BigInt.fromI32(1000);
+
+const PROTOCOL_ID = "worm-protocol-stats";
+const WITHDRAW_OPERATION_ID = `${PROTOCOL_ID}-withdraw`;
+
+describe("WORM withdraw tests", () => {
+  describe("1 event 1 tx", () => {
+    beforeAll(() => {
+      const event = createWithdrawEvent(WITHDRAW_TRANSACTION_HASH, BETH_CONTRACT_ADDRESS, AMOUNT);
+
+      handleWithdrawal(event);
+    });
+
+    afterAll(() => {
+      clearStore();
+    });
+
+    test("WormWithdraw created", () => {
+      assert.entityCount("WormWithdraw", 1);
+    });
+
+    test("WormWithdraw values stored correctly", () => {
+      const id = WITHDRAW_TRANSACTION_HASH.toHexString();
+
+      assert.fieldEquals("WormWithdraw", id, "value", AMOUNT.toString());
+      assert.fieldEquals("WormWithdraw", id, "txHash", WITHDRAW_TRANSACTION_HASH.toHexString());
+    });
+
+    test("Protocol stats updated", () => {
+      assert.fieldEquals("WormProtocolStats", PROTOCOL_ID, "totalTxCount", "1");
+    });
+
+    test("Withdraw operation stats updated", () => {
+      assert.fieldEquals("WormOperationStats", WITHDRAW_OPERATION_ID, "totalCount", "1");
+    });
+  });
+
+  describe("2 events 1 tx", () => {
+    beforeAll(() => {
+      const event1 = createWithdrawEvent(WITHDRAW_TRANSACTION_HASH, BETH_CONTRACT_ADDRESS, AMOUNT);
+      const event2 = createWithdrawEvent(WITHDRAW_TRANSACTION_HASH, BETH_CONTRACT_ADDRESS, AMOUNT);
+
+      handleWithdrawal(event1);
+      handleWithdrawal(event2);
+    });
+
+    afterAll(() => {
+      clearStore();
+    });
+
+    test("Only one WormWithdraw created", () => {
+      assert.entityCount("WormWithdraw", 1);
+    });
+
+    test("Protocol stats updated only once", () => {
+      assert.fieldEquals("WormProtocolStats", PROTOCOL_ID, "totalTxCount", "1");
+    });
+
+    test("Withdraw operation stats updated only once", () => {
+      assert.fieldEquals("WormOperationStats", WITHDRAW_OPERATION_ID, "totalCount", "1");
+    });
+  });
+
+  describe("2 events 2 txs", () => {
+    beforeAll(() => {
+      const event1 = createWithdrawEvent(WITHDRAW_TRANSACTION_HASH, BETH_CONTRACT_ADDRESS, AMOUNT);
+      const event2 = createWithdrawEvent(
+        Bytes.fromHexString("0xb16081f360e3847006db660bae1c6d1b2e17ec3b"),
+        BETH_CONTRACT_ADDRESS,
+        AMOUNT,
+      );
+
+      handleWithdrawal(event1);
+      handleWithdrawal(event2);
+    });
+
+    afterAll(() => {
+      clearStore();
+    });
+
+    test("Two WormWithdraw created", () => {
+      assert.entityCount("WormWithdraw", 2);
+    });
+
+    test("Protocol stats updated twice", () => {
+      assert.fieldEquals("WormProtocolStats", PROTOCOL_ID, "totalTxCount", "2");
+    });
+
+    test("Withdraw operation stats updated twice", () => {
+      assert.fieldEquals("WormOperationStats", WITHDRAW_OPERATION_ID, "totalCount", "2");
+    });
+  });
+
+  describe("event not from BETH contract", () => {
+    beforeAll(() => {
+      const event = createWithdrawEvent(
+        WITHDRAW_TRANSACTION_HASH,
+        Address.fromString("0x0000000000000000000000000000000000000002"),
+        AMOUNT,
+      );
+
+      handleWithdrawal(event);
+    });
+
+    afterAll(() => {
+      clearStore();
+    });
+
+    test("No WormWithdraw created", () => {
+      assert.entityCount("WormWithdraw", 0);
+    });
+
+    test("No Protocol stats created", () => {
+      assert.entityCount("WormProtocolStats", 0);
+    });
+
+    test("No Withdraw operation stats created", () => {
+      assert.entityCount("WormOperationStats", 0);
+    });
+  });
+});
